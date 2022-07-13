a0fa181411201c8792bae7455d904726754edc6a
candeluca <candelaohann@gmail.com>
2022-07-13 19:56:10 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'CoderHouse'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">TecnoImport</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Electrodomesticos</a>
+        <a class="nav-link" href="#">Tecnologia</a>
+        <a class="nav-link" href="#">Indumentaria</a>
+        <a class="nav-link disabled">Pesca</a>
+      </div>
+    </div>
+  </div>
+</nav>
     </div>
   );
 }
