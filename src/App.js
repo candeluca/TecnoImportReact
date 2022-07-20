20ff6f8b4b4719acc7d2b719fc20bff72bca8e9d
candeluca <candelaohann@gmail.com>
2022-07-20 16:58:50 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   const name = 'CoderHouse'
@@ -21,31 +22,17 @@ console.log ( condition ? 'verdadero' : 'falso')
 const numeros = [1, 2, 3, 4, 5, 6]
 const letras = ['a', 'b', 'c', 'd']
 
-const alfanumericos = [...numeros].push (letras)
-const alfanumericosVersionS = [...numeros, ...letras]
+const alfanumericosVersionVieja = numeros.concat(letras)
 
-console.log ( { alfanumericos })
+const alfanumericosVersionSugar = [...numeros, ...letras]
 
-console.log ( { alfanumericosVersionS })
+console.log ({ alfanumericosVersionVieja })
+
+console.log ({ alfanumericosVersionSugar })
 
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">TecnoImport</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Electrodomesticos</a>
-        <a class="nav-link" href="#">Tecnologia</a>
-        <a class="nav-link" href="#">Indumentaria</a>
-        <a class="nav-link disabled">Pesca</a>
-      </div>
-    </div>
-  </div>
-</nav>
+    <NavBar/> 
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+function App () {
+    return <>
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">TecnoImport</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link" href="#">Electrodomesticos</a>
+      <a class="nav-link" href="#">Tecnologia</a>
+      <a class="nav-link" href="#">Indumentaria</a>
+      <a class="nav-link" href="#">Pesca</a>
+      </div>
+    </div>
+    </div>
+    </nav>
+    </>
+}
+
+export default App;
