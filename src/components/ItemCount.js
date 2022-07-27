8b4228f7b33864994cff3191dd52e9a53262cd86
candeluca <candelaohann@gmail.com>
2022-07-27 19:46:13 -0300

    boton contador

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,11 +3,25 @@ import React, { useState } from "react";
 const ItemCount = ({initial, stock}) => {
     const [contador, setContador] = useState (initial)
     console.log('me renderice')
+
     const sumar = () => {
-        setContador(contador + 1)
+        if(contador < stock) {
+            setContador (contador + 1)
+        }
+        
     }
+
+    const restar = () => {
+        if (contador > initial) {
+            setContador (contador - 1)
+        }
+
+    }
+
+
     return (
         <div> 
+            <button onClick={restar}> - </button>
             <span> {contador} </span>
             <button onClick={sumar}> + </button>
         </div>
