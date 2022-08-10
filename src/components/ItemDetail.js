da40638bd23721a32d9225353da80e4c03bdbbe8
candeluca <candelaohann@gmail.com>
2022-08-09 21:31:23 -0300

    carrito

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -4,17 +4,24 @@ import ItemCount from "./ItemCount";
 
 const ItemDetail = ({producto}) => {
     const [count, setCount] = useState(0);
-    const [carrito, setCarrito] = useState (true);
+    const [carrito, setCarrito] = useState ();
     const {id, name, price, description, img} = producto;
     const {addToCart} = useContext ();
+    const navigate = useNavigate ();
 
     const onAdd = () => {
-        let productToBuy = { id, name, price, description, img}
+        let productToBuy = { id, name, price, description, img}; setCarrito (true); addToCart (productToBuy)
     }
 
 return (
     <div style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
     {producto.map ((producto)=> <Item key = {producto.id} producto = {producto}/>)}
+
+    <div> 
+        <button onClick={ () => navigate ('/productos')}>
+            Seguir comprando
+        </button>
+    </div>
     
     <ItemCount stock={10} initial={1} />
     </div>
